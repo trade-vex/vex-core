a8fffc9f19b25f2926d99c3110385359a5edaa23
RajasreePonnada <cs1230015@iitd.ac.in>
2025-06-26 17:45:19 +0530

    feat: Add JournalingProcessor for Persistent Logging

---
+++ processors/src/journaling.rs
@@ -0,0 +1,30 @@
+use common::cmd::MatcherTradeEvent;
+use common::cmd::OrderCommand;
+
+/// Responsible for writing all commands and events to a persistent log for durability.
+/// This is the Rust equivalent of `JournalingProcessor.java`.
+pub struct JournalingProcessor;
+
+impl JournalingProcessor {
+    pub fn new() -> Self {
+        Self
+    }
+
+    // Ring buffer Disruptor to JournalingProcessor - Logger(Excali-0)
+    pub fn journal_command(&self, cmd: &OrderCommand) {
+        println!("[Journal] Writing command to disk: ID {}", cmd.order_id);
+    }
+
+    pub fn journal_event(&self, event: &MatcherTradeEvent) {
+        println!(
+            "[Journal] Writing event to disk: Type {:?}",
+            event.event_type
+        );
+    }
+}
+
+impl Default for JournalingProcessor {
+    fn default() -> Self {
+        Self::new()
+    }
+}
