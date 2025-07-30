080bfc0710f2af408a1b37af3b99e7306bef5027
Suraj Choudhary <suraj@aeriuslabs.org>
2025-07-30 10:35:28 +0530

    chore: rename action to side

--- processors/src/risk_engine.rs
+++ processors/src/risk_engine.rs
@@ -91,16 +91,14 @@ impl RiskEngine {
                 "[RiskEngine] Found symbol_id spec: {:?} for symbol_id {}",
                 spec, cmd.symbol_id
             );
-            let required_funds = if cmd.action == Side::Bid {
+            let required_funds = if cmd.side == Side::Bid {
                 cmd.price * cmd.size
             } else {
                 // For ASK orders: need to lock the size (quantity being sold)
                 cmd.size
             };
 
-            let amount = required_funds;
-
-            if let Err(balance_error) = user_profile.lock_funds(cmd.user_id, cmd.market_id, amount) {
+            if !user_profile.hold_funds(spec, required_funds, cmd.side) {
                 warn!(
                     "[RiskEngine] Insufficient funds for user {} to place order {}",
                     cmd.user_id, cmd.order_id
