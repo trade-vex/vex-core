f27b18219d7462819a59896801c8eabfa668673a
Aditya <es1221211@iitd.ac.in>
2025-08-30 04:46:11 +0530

    chore : add second ring buffer and attach the matching handler macro to the event handler disruptor pipeline

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -24,34 +24,32 @@ macro_rules! create_risk_handler {
 macro_rules! create_risk_r2_handler {
     ($shard_id:expr, $risk_engines:expr) => {{
         let risk_engines_clone = $risk_engines.clone();
-        move |event: &ProcessedOrderEvent, _sequence: i64, _end_of_batch: bool| {
+        move |event: &MatcherTradeEvent, _sequence: i64, _end_of_batch: bool| {
             let num_shards = risk_engines_clone.len() as u64;
             let shard_mask = num_shards - 1;
 
-            for trade_event in &event.matcher_events {
-                // Route to risk engine shard for active order user
-                let active_order_user_id = trade_event.active_order_user_id;
-                let active_order_shard = (active_order_user_id & shard_mask) as usize;
+            // Route to risk engine shard for active order user
+            let active_order_user_id = event.active_order_user_id;
+            let active_order_shard = (active_order_user_id & shard_mask) as usize;
+            
+            // Only process if this event belongs to our shard
+            if active_order_shard == $shard_id {
+                if let Some(risk_engine_mutex) = risk_engines_clone.get($shard_id) {
+                    let mut risk_engine = risk_engine_mutex.lock();
+                    risk_engine.handle_event(event);
+                }
+            }
+
+            // Route to risk engine shard for maker user (if different)
+            let maker_user_id = event.maker_user_id;
+            if maker_user_id != active_order_user_id {
+                let maker_shard = (maker_user_id & shard_mask) as usize;
                 
                 // Only process if this event belongs to our shard
-                if active_order_shard == $shard_id {
+                if maker_shard == $shard_id {
                     if let Some(risk_engine_mutex) = risk_engines_clone.get($shard_id) {
                         let mut risk_engine = risk_engine_mutex.lock();
-                        risk_engine.handle_event(trade_event);
-                    }
-                }
-
-                // Route to risk engine shard for maker user (if different)
-                let maker_user_id = trade_event.maker_user_id;
-                if maker_user_id != active_order_user_id {
-                    let maker_shard = (maker_user_id & shard_mask) as usize;
-                    
-                    // Only process if this event belongs to our shard
-                    if maker_shard == $shard_id {
-                        if let Some(risk_engine_mutex) = risk_engines_clone.get($shard_id) {
-                            let mut risk_engine = risk_engine_mutex.lock();
-                            risk_engine.handle_event(trade_event);
-                        }
+                        risk_engine.handle_event(event);
                     }
                 }
             }
@@ -63,10 +61,8 @@ macro_rules! create_risk_r2_handler {
 macro_rules! create_event_handler {
     ($events_handler:expr) => {{
         let events_handler = $events_handler.clone();
-        move |event: &ProcessedOrderEvent, _sequence: i64, _end_of_batch: bool| {
-            for trade_event in &event.matcher_events {
-                events_handler.handle_event(trade_event.clone());
-            }
+        move |event: &MatcherTradeEvent, _sequence: i64, _end_of_batch: bool| {
+            events_handler.handle_event(event.clone());
         }
     }};
 }
@@ -75,26 +71,29 @@ macro_rules! create_event_handler {
 /// This eliminates code duplication while maintaining separate handlers for each shard
 #[macro_export]
 macro_rules! create_matching_handler {
-    ($shard_id:expr, $routers:expr, $processed_order_producer:expr) => {{
+    ($shard_id:expr, $routers:expr, $matcher_event_producer:expr) => {{
+        use disruptor::Producer;
         let router = $routers[$shard_id].clone();
-        let processed_order_producer = $processed_order_producer.clone();
+        let mut matcher_event_producer = $matcher_event_producer.clone();
 
         move |cmd: &OrderCommand, _sequence: i64, _end_of_batch: bool| {
             // Only lock during order processing
             let events = {
                 let mut router_guard = router.lock();
-                router_guard.process_order(cmd);
-                cmd.matcher_event.take()
+                // remove this lock eventually by some minor optimsations in orderbook and matching engine router
+                let mut order_cmd = cmd.clone();
+                router_guard.process_order(&mut order_cmd);
+                order_cmd.matcher_event.take()
             };  // Lock is released here - router is free for next order
 
             // Publish raw events directly
             if let Some(mut event_box) = events {
                 loop {
-                    let event = *event_box;
+                    let mut event = *event_box;
                     let next_event = event.next_event.take();
                     
                     // Publish raw event directly
-                    let _ = event_producer.publish_event(|published_event| {
+                    let _ = matcher_event_producer.publish(|published_event| {
                         *published_event = event;
                     });
                     
