c0c3f4f098c6a8a1ea48d2f0b01256daeef48d1c
Suraj Choudhary <suraj@aeriuslabs.org>
2025-08-30 19:31:30 +0530

    merge: refactor component to the upstream processor

--- xtask/src/bin/tests/test_server.rs
+++ xtask/src/bin/tests/test_server.rs
@@ -1,19 +1,8 @@
 use common::cmd::OrderCommand;
-<<<<<<< HEAD
-use disruptor::{BusySpin, ProcessorSettings, build_multi_producer};
-use std::io::Write;
-use std::{
-    env,
-    fs::OpenOptions,
-    sync::{Arc, Mutex},
-};
-use tracing::info;
-=======
 use std::io::Write;
 use disruptor::{build_multi_producer, BusySpin, ProcessorSettings};
 use tracing::info;
 use std::{env, fs::OpenOptions, sync::{Arc, Mutex}};
->>>>>>> chore/refactor-common-ob
 use vex_config::CoreNetworkingConfig;
 use vex_networking::server::VexCoreServer;
 
@@ -22,27 +11,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
     let server_host = env::var("VEX_SERVER_HOST").unwrap_or("127.0.0.1".to_string());
     let listen_port: u16 = env::var("VEX_SERVER_PORT")?.parse()?;
-<<<<<<< HEAD
-    println!("Server starting on port {}", listen_port);
-
-    let mut server_config = CoreNetworkingConfig::test_defaults();
-    server_config.local_address = server_host;
-    server_config.context_dir =
-        env::var("VEX_CONTEXT_DIR").unwrap_or("/dev/shm/aeron-test-server".to_string());
-    server_config.initial_port = listen_port;
-    server_config.initial_control_port = listen_port + 1;
-    let results_path = "/results/received_ids.txt";
-    let file = Arc::new(Mutex::new(
-        OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open(results_path)?,
-    ));
-
-    // A dummy consumer that just logs the received command
-    let producer = build_multi_producer(1024, || OrderCommand::default(), BusySpin)
-=======
     println!("Server starting on port {listen_port}");
 
     let mut server_config = CoreNetworkingConfig::test_defaults();
@@ -60,7 +28,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // A dummy consumer that just logs the received command
     let producer = build_multi_producer(1024, OrderCommand::default, BusySpin)
->>>>>>> chore/refactor-common-ob
         .pin_at_core(1)
         .handle_events_with({
             move |cmd: &OrderCommand, _, _| {
@@ -71,11 +38,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .handle_events_with({
             move |cmd: &OrderCommand, _, _| {
                 let mut f = file.lock().unwrap();
-<<<<<<< HEAD
-                writeln!(f, "{}", cmd.order_id).expect("Failed to write to results file");
-=======
                 writeln!(f, "{}", cmd.order_id).expect("Failed to write to results file");    
->>>>>>> chore/refactor-common-ob
                 info!("Server processing OrderCommand Core 2: {:?}", cmd);
             }
         })
