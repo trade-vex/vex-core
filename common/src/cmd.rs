1a02d4432469e83ea7a83ca179d42728b9085094
Aditya <es1221211@iitd.ac.in>
2025-08-30 21:50:03 +0530

    chore : add taker id in processed order event

--- common/src/cmd.rs
+++ common/src/cmd.rs
@@ -108,16 +108,18 @@ pub struct ProcessedOrderCommand {
     status: Status,
     order_id: u64,
     market_id: u32,
+    taker_id: u64,
     side: Side,
     events: Option<Box<MatcherTradeEvent>>,
 }
 
 impl ProcessedOrderCommand {
-    pub fn new(status: Status, order_id: u64, market_id: u32, taker_side: Side) -> Self {
+    pub fn new(status: Status, order_id: u64, taker_id: u64 ,market_id: u32, taker_side: Side) -> Self {
         Self {
             status,
             order_id,
             market_id,
+            taker_id,
             side: taker_side,
             events: None,
         }
@@ -129,6 +131,10 @@ impl ProcessedOrderCommand {
 
     pub fn order_id(&self) -> u64 {
         self.order_id
+    }
+
+    pub fn taker_id(&self) -> u64 {
+        self.taker_id
     }
 
     pub fn market_id(&self) -> u32 {
