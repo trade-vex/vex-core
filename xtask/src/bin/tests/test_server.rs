4da95d1034cce10729b0e81ece309dff16d2fb14
Suraj Choudhary <suraj@aeriuslabs.org>
2025-10-08 17:28:09 +0530

    chore: update x task tests

--- xtask/src/bin/tests/test_server.rs
+++ xtask/src/bin/tests/test_server.rs
@@ -1,14 +1,8 @@
-use common::OrderCommand;
-use disruptor::{BusySpin, ProcessorSettings, build_multi_producer};
-use std::io::Write;
-use std::{
-    env,
-    fs::OpenOptions,
-    sync::{Arc, Mutex},
-};
-use tracing::info;
+use common::{CoreMarketSpecification, MarketType, UserBalance};
+use hashbrown::HashMap;
+use std::env;
 use vex_config::CoreNetworkingConfig;
-use vex_networking::server::VexCoreServer;
+use vex_server::init_exchange;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read configuration from environment variables
@@ -26,81 +20,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     server_config.max_gateways = 15;
     server_config.max_connections_per_address = 10;
     let results_path = "/results/received_ids.txt";
-    let file = Arc::new(Mutex::new(
-        OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open(results_path)?,
-    ));
+    // let file = Arc::new(Mutex::new(
+    //     OpenOptions::new()
+    //         .create(true)
+    //         .write(true)
+    //         .truncate(true)
+    //         .open(results_path)?,
+    // ));
+
+    // let publications = Arc::new(vex_networking::server::GatewayPublications::new());
 
     // A dummy consumer that just logs the received command
-    let producer = build_multi_producer(1024, OrderCommand::default, BusySpin)
-        .pin_at_core(1)
-        .handle_events_with({
-            move |cmd: &mut OrderCommand, _, _| {
-                info!("Server received OrderCommand Core 1: {:?}", cmd);
-            }
-        })
-        .pin_at_core(2)
-        .handle_events_with({
-            move |cmd: &mut OrderCommand, _, _| {
-                let mut f = file.lock().unwrap();
-                writeln!(f, "{}", cmd.order_id).expect("Failed to write to results file");
-                info!("Server processing OrderCommand Core 2: {:?}", cmd);
-            }
-        })
-        .and_then()
-        .pin_at_core(3)
-        .handle_events_with({
-            move |cmd: &mut OrderCommand, _, _| {
-                info!("Server processing OrderCommand Core 3: {:?}", cmd);
-            }
-        })
-        .build();
+    let mut specs = HashMap::new();
+    let base_asset_id = 1;
+    let quote_asset_id = 2;
+    // Market ID: base asset in lower 16 bits, quote in upper 16
+    let market_id = ((quote_asset_id as u32) << 16) | (base_asset_id as u32);
+    add_spec(market_id, &mut specs);
 
-    let mut server = VexCoreServer::new(server_config, producer)?;
+    let (mut core_engine, producer, risk_engines) = init_exchange(specs);
+    let risk_engines = risk_engines.unwrap();
+    risk_engines[1].set_balance(1, base_asset_id, UserBalance::new(10000000, 0));
+    risk_engines[1].set_balance(1, quote_asset_id, UserBalance::new(10000000, 0));
+    risk_engines[2].set_balance(2, base_asset_id, UserBalance::new(10000000, 0));
+    risk_engines[2].set_balance(2, quote_asset_id, UserBalance::new(10000000, 0));
 
-    // Start the server's event loop
-    println!("Server listening for messages...");
-    server.start()?; // This will run indefinitely
+    let t = core_engine.run(producer, server_config);
 
+    t.join().unwrap();
     Ok(())
 }
-// use common::CoreMarketSpecificationBuilder;
-// use hashbrown::HashMap;
-// use std::env;
-// use vex_config::CoreNetworkingConfig;
-// use vex_networking::server::VexCoreServer;
-// use vex_server::init_exchange;
 
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // Read configuration from environment variables
-//     tracing_subscriber::fmt::init();
-//     let server_host = env::var("VEX_SERVER_HOST").unwrap_or("127.0.0.1".to_string());
-//     let listen_port: u16 = env::var("VEX_SERVER_PORT")?.parse()?;
-//     println!("Server starting on port {listen_port}");
-
-//     let mut server_config = CoreNetworkingConfig::test_defaults();
-//     server_config.local_address = server_host;
-//     server_config.context_dir =
-//         env::var("VEX_CONTEXT_DIR").unwrap_or("/dev/shm/aeron-test-server".to_string());
-//     server_config.initial_port = listen_port;
-//     server_config.initial_control_port = listen_port + 1;
-//     server_config.max_gateways = 15;
-//     server_config.max_connections_per_address = 10;
-
-//     let mut symbol_specs = HashMap::new();
-//     let btcusd_market = CoreMarketSpecificationBuilder::default()
-//         .market_id(10)
-//         .build()
-//         .unwrap();
-//     symbol_specs.insert(10_u32, btcusd_market);
-//     let (_, producer) = init_exchange(symbol_specs);
-//     let mut server = VexCoreServer::new(server_config, producer)?;
-
-//     // Start the server's event loop
-//     println!("Server listening for messages...");
-//     server.start()?;
-//     Ok(())
-// }
+fn add_spec(market_id: u32, specs: &mut HashMap<u32, CoreMarketSpecification>) {
+    specs.insert(
+        market_id,
+        CoreMarketSpecification::builder()
+            .market_id(market_id)
+            .market_type(MarketType::Spot)
+            .maker_fee(10) // 0.1%
+            .taker_fee(20) // 0.2%
+            .slippage(5)
+            .build()
+            .unwrap(),
+    );
+}
