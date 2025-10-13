788f3eec53a824a3690bff615b9286e1a3eb16b3
Suraj Choudhary <suraj@aeriuslabs.org>
2025-10-13 15:39:39 +0530

    chore: add processed to status for deposit withdrawals

--- order/src/status.rs
+++ order/src/status.rs
@@ -7,6 +7,7 @@ pub enum Status {
     PartiallyFilled = 0x3_u8, 
     Filled = 0x4_u8, 
     Processing = 0x5_u8, 
+    Processed = 0x6_u8, 
     #[default]
     NullVal = 0xff_u8, 
 }
@@ -20,6 +21,7 @@ impl From<u8> for Status {
             0x3_u8 => Self::PartiallyFilled, 
             0x4_u8 => Self::Filled, 
             0x5_u8 => Self::Processing, 
+            0x6_u8 => Self::Processed, 
             _ => Self::NullVal,
         }
     }
@@ -34,6 +36,7 @@ impl From<Status> for u8 {
             Status::PartiallyFilled => 0x3_u8, 
             Status::Filled => 0x4_u8, 
             Status::Processing => 0x5_u8, 
+            Status::Processed => 0x6_u8, 
             Status::NullVal => 0xff_u8,
         }
     }
@@ -50,6 +53,7 @@ impl core::str::FromStr for Status {
             "PartiallyFilled" => Ok(Self::PartiallyFilled), 
             "Filled" => Ok(Self::Filled), 
             "Processing" => Ok(Self::Processing), 
+            "Processed" => Ok(Self::Processed), 
             _ => Ok(Self::NullVal),
         }
     }
@@ -64,6 +68,7 @@ impl core::fmt::Display for Status {
             Self::PartiallyFilled => write!(f, "PartiallyFilled"), 
             Self::Filled => write!(f, "Filled"), 
             Self::Processing => write!(f, "Processing"), 
+            Self::Processed => write!(f, "Processed"), 
             Self::NullVal => write!(f, "NullVal"),
         }
     }
