0738eddc2ac5a46d658146a250dbade6d6cc276d
Rahul Ghangas <rahulghangas0@gmail.com>
2025-12-06 01:26:11 +0530

    fix: do not process rejected orders

--- orderbook/src/lib.rs
+++ orderbook/src/lib.rs
@@ -225,6 +225,11 @@ impl<Ask: BookSide, Bid: BookSide> OrderBook<Ask, Bid> {
     ///    All the contraints are NOT checked in the ORDERBOOK, must be guaranteed by upstream systems
     ///    They are not included here to avoid redundant checks that are already made
     pub fn place_order(&mut self, cmd: &mut OrderCommand, price_cache: Arc<PriceCache>) {
+        // Skip processing if the order is already rejected
+        if cmd.status == Status::Rejected {
+            return;
+        }
+
         match cmd.time_in_force {
             TimeInForce::Gtc => {
                 // Handle GTC (Good 'Til Canceled) orders
